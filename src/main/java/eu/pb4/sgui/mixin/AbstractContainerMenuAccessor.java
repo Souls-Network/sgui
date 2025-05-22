103d3ee0729450977371c42a537c3c5cce66dfb7
Jean-Baptiste Lemée <git@lemee.co>
2025-05-21 14:40:09 -1000

    Switch to mojang offical mapping

---
+++ src/main/java/eu/pb4/sgui/mixin/AbstractContainerMenuAccessor.java
@@ -0,0 +1,12 @@
+package eu.pb4.sgui.mixin;
+
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.RemoteSlot;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(AbstractContainerMenu.class)
+public interface AbstractContainerMenuAccessor {
+    @Accessor
+    RemoteSlot getRemoteCarried();
+}
---
+++ src/main/java/eu/pb4/sgui/mixin/ServerPlayerAccessor.java
@@ -0,0 +1,12 @@
+package eu.pb4.sgui.mixin;
+
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+@Mixin(ServerPlayer.class)
+public interface ServerPlayerAccessor {
+    @Invoker
+    void callInitMenu(AbstractContainerMenu screenHandler);
+}
